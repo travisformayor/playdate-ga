e9fa88ad3166e59b94830fe1527f5fc396a66e58
Allen Wright <allenwrightkc@gmail.com>
2019-04-15 10:00:37 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+
+// BodyParser Middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// Serve static assets
+app.use(express.static(__dirname + '/public'));
+
+// ROOT route
+app.get('/', (req, res) => {
+  res.sendFile('views/index.html', {root: __dirname});
+});
+
+// start server
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
