3e391a70cad5c2adecd7fab308881ec99b9a3ce4
Travis <travisformayor@users.noreply.github.com>
2019-04-16 13:21:38 -0700

    added all pets to seed

--- seed.js
+++ seed.js
@@ -1,17 +1,40 @@
 const db = require('./models');
 
-const seedPets = [
-  {
-    loginId: 1,
-    name: 'Godzilla',
-    type: 'monster',
-    age: 1267,
-    bio: 'Young at heart monster who likes stomping through downtown Tokyo. ',
-    img: '/images/godzilla.jpg',
-    likes: ['user3', 'user6']
-  }
+// const seedPets = [
+//   {
+//     loginId: 1,
+//     name: 'Godzilla',
+//     type: 'monster',
+//     age: 1267,
+//     bio: 'Young at heart monster who likes stomping through downtown Tokyo. ',
+//     img: 'godzilla.jpg',
+//     likes: []
+//   }
+// ];
+
+const petSeed = [
+  {loginId: 1, name: 'Badger', img: 'badger.jpg', age: '6 years', type: 'Dog', bio: 'Woof. Placeholder Bio.', likes: []},
+  {loginId: 2, name: 'Bailey', img: 'bailey.jpg', age: '11 years', type: 'Dog', bio: 'Woof. Placeholder Bio.', likes: []},
+  {loginId: 3, name: 'Bedelia', img: 'bedelia.jpg', age: '7 months', type: 'Bearded Dragon', bio: 'Placeholder Bio.', likes: []},
+  {loginId: 4, name: 'Coraline', img: 'coraline.jpg', age: '2 years', type: 'Cat', bio: 'Meow. Placeholder Bio.', likes: []},
+  {loginId: 5, name: 'Dakota', img: 'dakota.jpg', age: '11 years', type: 'Dog', bio: 'Woof. Placeholder Bio.', likes: []},
+  {loginId: 6, name: 'Frodo', img: 'frodo.jpg', age: '6 months', type: 'Gecko', bio: 'Placeholder Bio.', likes: []},
+  {loginId: 7, name: 'Indiana Bones', img: 'indianabones.jpg', age: '5 months', type: 'Dog', bio: 'Woof. Placeholder Bio.', likes: []},
+  {loginId: 8, name: 'King Bee', img: 'kingbee.jpg', age: '1 week', type: 'Ducky', bio: 'Quack. Placeholder Bio.', likes: []},
+  {loginId: 9, name: 'Lucky', img: 'lucky.jpg', age: '5 years', type: 'Cat', bio: 'Meow. Placeholder Bio.', likes: []},
+  {loginId: 10, name: 'Moxie', img: 'moxie.jpg', age: '', type: 'Dog', bio: 'Woof. Placeholder Bio.', likes: []},
+  {loginId: 11, name: 'Nadia', img: 'nadia.jpg', age: '7 years', type: 'Cat', bio: 'Meow. Placeholder Bio.', likes: []},
+  {loginId: 12, name: 'Oreo', img: 'oreo.jpg', age: '5 years', type: 'Cat', bio: 'Meow. Placeholder Bio.', likes: []},
+  {loginId: 13, name: 'Orpheus', img: 'orpheus.jpg', age: '2 years', type: 'Tortoise', bio: 'Placeholder Bio.', likes: []},
+  {loginId: 14, name: 'Penelope', img: 'penelope.jpg', age: '2 years', type: 'Cat', bio: 'Meow. Placeholder Bio.', likes: []},
+  {loginId: 15, name: 'Persephone', img: 'persephone.jpg', age: '4 months', type: 'Gecko', bio: 'Placeholder Bio.', likes: []},
+  {loginId: 16, name: 'Sabriel', img: 'sabriel.jpg', age: '7 years', type: 'Ball Python', bio: 'Placeholder Bio.', likes: []},
+  {loginId: 17, name: 'Sierra', img: 'sierra.jpg', age: '8 years', type: 'Dog', bio: 'Woof. Placeholder Bio.', likes: []},
+  {loginId: 18, name: 'Smaug', img: 'smaug.jpg', age: '10 months', type: 'Bearded Dragon', bio: 'Placeholder Bio.', likes: []},
+  {loginId: 19, name: 'Venus', img: 'venus.jpg', age: '4 months', type: 'Rosy Boa', bio: 'Placeholder Bio.', likes: []}
 ];
 
+// DELETE all collection records
 // DELETE all Pets
 db.Pet.deleteMany({}, (err, deletedPets) => {
   if (err) {
