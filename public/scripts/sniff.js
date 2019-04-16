fbb92f127f472d638d45abbce4d633119f566b76
Allen Wright <allenwrightkc@gmail.com>
2019-04-16 15:56:24 -0700

    fixed carousel arrows, fixed carousel rotation

--- public/scripts/sniff.js
+++ public/scripts/sniff.js
@@ -1,3 +1,13 @@
 console.log('sanity check');
 
 // generate array of all pets
+function getAllPets() {
+  const api = '/api/pets';
+  $.ajax({
+    method: 'GET',
+    url: api,
+    success: handleSuccess,
+    error: handleError
+  });
+
+}
