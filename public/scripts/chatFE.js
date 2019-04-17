ff5b28ce56549905725dae515ad66fd5e5eb6c5d
mpetty <michael.petty@gmail.com>
2019-04-17 15:30:26 -0700

    working html/css for chat; ready for events and data

--- public/scripts/chatFE.js
+++ public/scripts/chatFE.js
@@ -1,30 +1,36 @@
-// get the profile loginId from the URL
-const url = window.location.href;
-const id = parseInt(url.substring(url.lastIndexOf('/') + 1));
+$(document).ready(function(){
+  // get the profile loginId from the URL
+  const url = window.location.href;
+  const id = parseInt(url.substring(url.lastIndexOf('/') + 1));
 
-getMatchesById(id);
+  getMatchesById(id);
 
-// generate array of all matches/chats for loginId
-function getMatchesById(loginid) {
-  const api = `/api/matches/${loginid}`;
-  $.ajax({
-    method: 'GET',
-    url: api,
-    success: handleSuccess,
-    error: handleError
-  });
+  // generate array of all matches/chats for loginId
+  function getMatchesById(loginid) {
+    const api = `/api/matches/${loginid}`;
+    $.ajax({
+      method: 'GET',
+      url: api,
+      success: handleSuccess,
+      error: handleError
+    });
 
-  function handleSuccess(res) {
-    if (res[0]) {
-      console.log('do stuff');
-    } else {
-      console.log(`in handleSuccess but no match info: ${res}`)
-      handleError(res);
+    function handleSuccess(res) {
+      if (res[0].match) {
+        console.log('do stuff');
+      } else {
+        handleError(res);
+      }
+    }
+
+    function handleError(err) {
+      let error = `<h1>Error retrieving pet information. Please try again.</h1>`;
+      $('main').append(error);
     }
   }
 
-  function handleError(err) {
-    let error = `<h1>Error retrieving pet information. Please try again.</h1>`;
-    $('main').append(error);
-  }
-}
+
+  $('#action_menu_btn').click(function(){
+    $('.action_menu').toggle();
+  })
+})
