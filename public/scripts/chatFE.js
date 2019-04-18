91823b273ce866e3f83fc9b5e4e2d0b01b7f7f12
mpetty <michael.petty@gmail.com>
2019-04-17 17:26:03 -0700

    added html blocks to app.js; still needs dynamic data

--- public/scripts/chatFE.js
+++ public/scripts/chatFE.js
@@ -17,7 +17,63 @@ $(document).ready(function(){
 
     function handleSuccess(res) {
       if (res[0].match) {
-        console.log('do stuff');
+        console.log(res);
+        // TODO: switch to db img
+        //$('nav .profile-icon').css('background-image', `url(/images/thumb/${pet.img})`);
+        $('nav .profile-icon').css('background-image', `url(/images/thumb/moxie.jpg)`);
+
+        //build matches
+        $('.contacts').append(`
+          <li data-with-id="someId">
+          <div class="d-flex bd-highlight">
+            <div class="img_cont">
+              <img src="/images/thumb/badger.jpg" class="rounded-circle user_img">
+            </div>
+            <div class="user_info">
+              <span>Badger</span>
+            </div>
+          </div>
+        </li>`
+      )
+
+      //build chat header
+      $('.msg_head').prepend(`
+          <div class="d-flex bd-highlight">
+            <div class="img_cont">
+              <img src="/images/thumb/bailey.jpg" class="rounded-circle user_img">
+            </div>
+            <div class="user_info">
+              <span>DYNAMIC Chat with Bailey</span>
+            </div>
+          </div>`
+        )
+
+        //build message from match
+        $('.msg_card_body').append(`
+          <div class="d-flex justify-content-start mb-4">
+            <div class="img_cont_msg">
+              <img src="/images/thumb/bailey.jpg" class="rounded-circle user_img_msg">
+            </div>
+            <div class="msg_cotainer">
+              Hi, how are you moxie?
+              <span class="msg_time">8:40 AM, Today</span>
+            </div>
+          </div>`
+        )
+
+        //build message from user
+        $('.msg_card_body').append(`
+            <div class="d-flex justify-content-end mb-4">
+              <div class="msg_cotainer_send">
+                Hi bailey i am good tnx how about you?
+                <span class="msg_time_send">8:55 AM, Today</span>
+              </div>
+              <div class="img_cont_msg">
+            <img src="/images/thumb/moxie.jpg" class="rounded-circle user_img_msg">
+              </div>
+            </div>`
+          )
+
       } else {
         handleError(res);
       }
